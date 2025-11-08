5dfd6b7dcc97b809187ca1108516b5931ddf82cf
Vishal Gupta <vguptakota1998@gmail.com>
2025-11-08 22:17:57 +0530

    chore: add firebase and swif t sdk deps

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "integration-swift-firebase",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12),
+        .tvOS(.v15),
+        .watchOS(.v8)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -12,11 +18,21 @@ let package = Package(
             targets: ["integration-swift-firebase"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/firebase/firebase-ios-sdk", .upToNextMajor(from: "12.5.0")),
+        // we will change the dependency rudder-sdk-swift
+        .package(url: "https://github.com/rudderlabs/rudder-sdk-swift.git", branch: "chore/test-firebase-integration")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "integration-swift-firebase"
+            name: "integration-swift-firebase",
+            dependencies: [
+                .product(name: "FirebaseAnalytics", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseCore", package: "firebase-ios-sdk"),
+                .product(name: "RudderStackAnalytics", package: "rudder-sdk-swift")
+            ]
         ),
         .testTarget(
             name: "integration-swift-firebaseTests",
