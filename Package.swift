eba71ea5935f9d3a063568de552f217c6e8fbcc6
Vishal Gupta <vguptakota1998@gmail.com>
2025-11-09 20:09:19 +0530

    feat: add example app and rename package

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "integration-swift-firebase",
-            targets: ["integration-swift-firebase"]
+            name: "RudderIntegrationFirebase",
+            targets: ["RudderIntegrationFirebase"]
         )
     ],
     dependencies: [
@@ -27,7 +27,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "integration-swift-firebase",
+            name: "RudderIntegrationFirebase",
             dependencies: [
                 .product(name: "FirebaseAnalytics", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseCore", package: "firebase-ios-sdk"),
@@ -35,8 +35,8 @@ let package = Package(
             ]
         ),
         .testTarget(
-            name: "integration-swift-firebaseTests",
-            dependencies: ["integration-swift-firebase"]
+            name: "RudderIntegrationFirebaseTests",
+            dependencies: ["RudderIntegrationFirebase"]
         )
     ]
 )
